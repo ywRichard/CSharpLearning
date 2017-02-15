8652e79a92d7c756e8aa10c3a66c85ffc5577f0b
Victor Yao <ywfire0620@163.com>
2017-02-15 19:11:38 +0800

    Created The Project

---
+++ 06_CSTestDemo/001_TestPathClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace _01_TestPathClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\S2I\Desktop\Victor's S2I\06_Convert Objects\Primitive Objects\Image\VJD_Image.vxml";
+
+            string fileName = Path.GetFileNameWithoutExtension(path);
+            string dir = Path.GetDirectoryName(path);
+            string newPath = Path.Combine(dir, fileName,"111.jpg");
+
+            Console.WriteLine(newPath);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 06_CSTestDemo/103_ReadXML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ReadXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xdoc = XDocument.Load(@"C:\Users\S2I\Desktop\read.xml");
+
+            IEnumerable<XElement> eles = xdoc.Elements();
+
+            foreach (var item in eles)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine(xdoc.Root);
+
+            Console.ReadLine();
+        }
+    }
+}
