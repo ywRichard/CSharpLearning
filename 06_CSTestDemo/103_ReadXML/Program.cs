9ddcafba55f12cab70fde5dce67a5028b36e803f
Victor <ywfire0620@163.com>
2017-03-09 00:50:16 +0800

    Merge projects_1

--- 06_CSTestDemo/103_ReadXML/Program.cs
+++ 06_CSTestDemo/103_ReadXML/Program.cs
@@ -10,6 +10,7 @@ namespace ReadXML
     {
         static void Main(string[] args)
         {
+            #region
             XDocument xdoc = XDocument.Load(@"C:\Users\S2I\Desktop\read.xml");
 
             IEnumerable<XElement> eles = xdoc.Elements();
@@ -20,6 +21,33 @@ namespace ReadXML
             }
 
             Console.WriteLine(xdoc.Root);
+            #endregion
+
+            #region Root
+            //XDocument XDoc = XDocument.Load("ReadXml.xml");
+
+            //XElement xele = XDoc.Root;
+
+            ////Console.WriteLine(xele);
+
+            //IEnumerable<XElement> eles = xele.Elements();
+
+            //foreach (XElement elesClass in eles)
+            //{
+            //    //Console.WriteLine(elesClass.Name);
+            //    //Console.WriteLine(elesClass.Value);
+
+            //    foreach (var elesStudent in elesClass.Elements())
+            //    {
+            //        Console.WriteLine(elesStudent.Attribute("id").Value);//student元素的属性id的值
+            //        Console.WriteLine(elesStudent.Name);//student元素的名字
+
+            //        Console.WriteLine(elesStudent.FirstAttribute.Value);//Student元素第一个属性的值
+            //        Console.WriteLine(elesStudent.Element("name").Value);//按名称读取子元素的值
+            //        //Console.WriteLine(elesStudent.FirstNode);//Student元素第一个节点
+            //    }
+            //}
+            #endregion
 
             Console.ReadLine();
         }
