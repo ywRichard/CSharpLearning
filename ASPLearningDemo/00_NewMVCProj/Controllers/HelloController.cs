77fd9d65fff0d56b36f82f7f8327e87a782dcc07
Victor <ywfire0620@163.com>
2017-03-20 22:28:26 +0800

    Add ASP.Net Demo
    Hello Controller

---
+++ ASPLearningDemo/00_NewMVCProj/Controllers/HelloController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _00_NewMVCProj.Controllers
+{
+    public class HelloController : Controller
+    {
+        //
+        // GET: /Hello/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "Hello Controller";
+        //}
+
+        /// <summary>
+        /// 无参函数
+        /// </summary>
+        /// <returns></returns>
+        //public string Welcome()
+        //{
+        //    return "Welcome!";
+        //}
+
+        public string Welcome(string name = "China")
+        {
+            return "Welcome" + Server.HtmlEncode(name);
+            //return "Welcome" + HttpUtility.HtmlEncode(name);
+        }
+	}
+}
