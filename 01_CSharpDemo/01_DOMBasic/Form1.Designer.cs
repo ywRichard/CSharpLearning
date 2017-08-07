8ff4eab0229325b3bfeed55f89152453a5eb0d43
Victor's Office <ywfire0620@163.com>
2017-08-07 17:38:27 +0800

    Update XML

---
+++ 01_CSharpDemo/01_DOMBasic/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace _01_DOMBasic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoop = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCreateNode = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnSingle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoop
+            // 
+            this.btnLoop.Location = new System.Drawing.Point(320, 20);
+            this.btnLoop.Name = "btnLoop";
+            this.btnLoop.Size = new System.Drawing.Size(75, 23);
+            this.btnLoop.TabIndex = 0;
+            this.btnLoop.Text = "Loop";
+            this.btnLoop.UseVisualStyleBackColor = true;
+            this.btnLoop.Click += new System.EventHandler(this.btnLoop_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 22);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(289, 199);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnCreateNode
+            // 
+            this.btnCreateNode.Location = new System.Drawing.Point(320, 61);
+            this.btnCreateNode.Name = "btnCreateNode";
+            this.btnCreateNode.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateNode.TabIndex = 2;
+            this.btnCreateNode.Text = "Insert";
+            this.btnCreateNode.UseVisualStyleBackColor = true;
+            this.btnCreateNode.Click += new System.EventHandler(this.btnCreateNode_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(320, 100);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 3;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnSingle
+            // 
+            this.btnSingle.Location = new System.Drawing.Point(307, 140);
+            this.btnSingle.Name = "btnSingle";
+            this.btnSingle.Size = new System.Drawing.Size(88, 23);
+            this.btnSingle.TabIndex = 4;
+            this.btnSingle.Text = "SelectSingle";
+            this.btnSingle.UseVisualStyleBackColor = true;
+            this.btnSingle.Click += new System.EventHandler(this.btnSingle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 262);
+            this.Controls.Add(this.btnSingle);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnCreateNode);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnLoop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoop;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCreateNode;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnSingle;
+    }
+}
+
---
+++ 01_CSharpDemo/01_DOMBasic/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace _01_DOMBasic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoop_Click(object sender, EventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(@"../../Book.xml");
+            txtResult.Text = FormatText(document.DocumentElement as XmlNode, "", "");
+            
+        }
+
+        private string FormatText(XmlNode node, string text, string indent)
+        {
+            if (node is XmlText)
+            {
+                text += node.Value;
+                return text;
+            }
+
+            if (string.IsNullOrEmpty(indent))
+                indent = "";
+            else
+                text += "\r\n" + indent;
+
+            if (node is XmlComment)
+            {
+                text += node.OuterXml;
+                return text;
+            }
+
+            text += "<" + node.Name;
+            if (node.Attributes.Count>0)
+                AddAttributes(node, ref text);
+
+            if (node.HasChildNodes)
+            {
+                text += ">";
+                foreach (XmlNode child in node.ChildNodes)
+                    text = FormatText(child, text, indent + " ");
+
+                if (node.ChildNodes.Count==1&&
+                    (node.FirstChild is XmlText || node.FirstChild is XmlComment))
+                    text += "</" + node.Name + ">";
+                else
+                    text += "\r\n" + indent + "</" + node.Name + ">";
+            }
+            else
+                text += "/>";
+
+            return text;
+        }
+
+        private void AddAttributes(XmlNode node, ref string text)
+        {
+            foreach (XmlAttribute xa in node.Attributes)
+            {
+                text += " " + xa.Name + "='" + xa.Value + "'";
+            }
+        }
+
+        private void btnCreateNode_Click(object sender, EventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(@"../../Book.xml");
+
+            XmlElement root = document.DocumentElement;
+
+            XmlElement newBook = document.CreateElement("book");
+            XmlElement newTitle = document.CreateElement("title");
+            XmlElement newAuthor = document.CreateElement("author");
+            XmlElement newCode = document.CreateElement("code");
+            XmlText title = document.CreateTextNode("C# Basic");
+            XmlText author = document.CreateTextNode("Karin");
+            XmlText code = document.CreateTextNode("123456");
+            XmlComment comment = document.CreateComment("This is Comment");
+
+            newBook.AppendChild(comment);
+            newBook.AppendChild(newTitle);
+            newBook.AppendChild(newAuthor);
+            newBook.AppendChild(newCode);
+            newTitle.AppendChild(title);
+            newAuthor.AppendChild(author);
+            newCode.AppendChild(code);
+            root.InsertAfter(newBook, root.FirstChild);
+            document.Save(@"../../Book.xml");
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(@"../../Book.xml");
+
+            XmlElement root = document.DocumentElement;
+
+            if (root.HasChildNodes)
+            {
+                XmlNode book = root.LastChild;
+                root.RemoveChild(book);
+                document.Save(@"../../Book.xml");
+            }
+        }
+
+        private void btnSingle_Click(object sender, EventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(@"../../Book.xml");
+
+            XmlElement root = document.DocumentElement;
+
+            XmlNode node = root.FirstChild.SelectSingleNode("title");
+            txtResult.Text = node.InnerXml;
+
+            XmlNode nodeBook = root.SelectSingleNode("book");
+            txtResult.Text += nodeBook.InnerXml;
+        }
+    }
+}
