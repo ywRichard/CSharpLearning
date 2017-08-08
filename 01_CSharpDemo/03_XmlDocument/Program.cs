a93cd51c356ecca35bc1206bc580b50b2dbcbfce
Victor's Office <ywfire0620@163.com>
2017-08-08 19:58:32 +0800

    update

---
+++ 01_CSharpDemo/03_XmlDocument/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _03_XmlDocument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //增删改查
+            XmlDocument document = new XmlDocument();
+            document.Load(@"../../Book.xml");
+            var root = document.DocumentElement;
+
+            foreach (var node in root.ChildNodes)
+            {
+                if (node is XmlElement)
+                {
+                    //查
+                    var element = node as XmlElement;
+                    if (element.SelectSingleNode("author").InnerText == "Karin")
+                    {
+                        Console.WriteLine(element.SelectSingleNode("title").InnerText);
+                    }
+
+                    //删
+                    foreach (var subNode in element.ChildNodes)
+                    {
+                        if (subNode is XmlComment)
+                        {
+                            var comment = subNode as XmlComment;
+                            if (comment.Data == "For Delete")
+                            {
+                                comment.ParentNode.RemoveChild(comment);
+                                Console.WriteLine("Deleted this comment:{0}", comment.Data);
+                                //可以边操作，边保存
+                                document.Save(@"../../Book.xml");
+                            }
+                        }
+                    }
+                }
+            }
+    
+            foreach (var node in root.ChildNodes)
+            {
+                if ((node as XmlElement).Name == "book")
+                {
+                    var book = node as XmlElement;
+                    book.SelectSingleNode("author");
+                    //改
+                    if (book.SelectSingleNode("author").InnerXml=="Hello")
+                    {
+                        book.SelectSingleNode("code").InnerXml = "####";
+                        document.Save(@"../../Book.xml");
+                        Console.WriteLine("Changed the Hello code");
+                    }
+                }
+            }
+
+            var newBook = document.CreateElement("New_Book");
+            var newAuther = document.CreateElement("David");
+            newBook.AppendChild(newAuther);
+            root.InsertAfter(newBook, root.FirstChild);
+
+            document.Save(@"../../Book.xml");
+        }
+    }
+}
---
+++ 01_CSharpDemo/04_XmlNavigator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace _04_XmlNavigator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var document = new XPathDocument(@"../../Book.xml");
+            var root = document.CreateNavigator();
+            IterationXML(root);
+            Console.WriteLine("Finished");
+        }
+
+        private static string IterationXML(XPathNavigator element)
+        {
+            if (element.HasChildren)
+            {
+                foreach (var item in element.SelectChildren(XPathNodeType.All))
+                {
+
+                }
+            }
+        }
+    }
+
+}
