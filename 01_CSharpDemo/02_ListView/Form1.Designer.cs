8a98ea0a6605524f91788a2b758550de0ae52912
Victor's MacOS <ywfire0620@163.com>
2017-08-09 22:47:29 +0800

    Update Code

---
+++ 01_CSharpDemo/02_ListView/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace _02_ListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lvFilesFolders = new System.Windows.Forms.ListView();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbLargeIcon = new System.Windows.Forms.RadioButton();
+            this.rbSmallIcon = new System.Windows.Forms.RadioButton();
+            this.rbList = new System.Windows.Forms.RadioButton();
+            this.rbDetails = new System.Windows.Forms.RadioButton();
+            this.rbTitle = new System.Windows.Forms.RadioButton();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.imgListSmall = new System.Windows.Forms.ImageList(this.components);
+            this.imgListLarge = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvFilesFolders
+            // 
+            this.lvFilesFolders.Location = new System.Drawing.Point(26, 34);
+            this.lvFilesFolders.Name = "lvFilesFolders";
+            this.lvFilesFolders.Size = new System.Drawing.Size(368, 230);
+            this.lvFilesFolders.TabIndex = 0;
+            this.lvFilesFolders.UseCompatibleStateImageBehavior = false;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(24, 19);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(77, 12);
+            this.lblPath.TabIndex = 1;
+            this.lblPath.Text = "Current Path";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbTitle);
+            this.groupBox1.Controls.Add(this.rbDetails);
+            this.groupBox1.Controls.Add(this.rbList);
+            this.groupBox1.Controls.Add(this.rbSmallIcon);
+            this.groupBox1.Controls.Add(this.rbLargeIcon);
+            this.groupBox1.Location = new System.Drawing.Point(426, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(138, 229);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "View Mode";
+            // 
+            // rbLargeIcon
+            // 
+            this.rbLargeIcon.AutoSize = true;
+            this.rbLargeIcon.Location = new System.Drawing.Point(28, 26);
+            this.rbLargeIcon.Name = "rbLargeIcon";
+            this.rbLargeIcon.Size = new System.Drawing.Size(77, 16);
+            this.rbLargeIcon.TabIndex = 0;
+            this.rbLargeIcon.TabStop = true;
+            this.rbLargeIcon.Text = "LargeIcon";
+            this.rbLargeIcon.UseVisualStyleBackColor = true;
+            // 
+            // rbSmallIcon
+            // 
+            this.rbSmallIcon.AutoSize = true;
+            this.rbSmallIcon.Location = new System.Drawing.Point(28, 51);
+            this.rbSmallIcon.Name = "rbSmallIcon";
+            this.rbSmallIcon.Size = new System.Drawing.Size(77, 16);
+            this.rbSmallIcon.TabIndex = 1;
+            this.rbSmallIcon.TabStop = true;
+            this.rbSmallIcon.Text = "SmallIcon";
+            this.rbSmallIcon.UseVisualStyleBackColor = true;
+            // 
+            // rbList
+            // 
+            this.rbList.AutoSize = true;
+            this.rbList.Location = new System.Drawing.Point(28, 76);
+            this.rbList.Name = "rbList";
+            this.rbList.Size = new System.Drawing.Size(47, 16);
+            this.rbList.TabIndex = 2;
+            this.rbList.TabStop = true;
+            this.rbList.Text = "List";
+            this.rbList.UseVisualStyleBackColor = true;
+            // 
+            // rbDetails
+            // 
+            this.rbDetails.AutoSize = true;
+            this.rbDetails.Location = new System.Drawing.Point(28, 101);
+            this.rbDetails.Name = "rbDetails";
+            this.rbDetails.Size = new System.Drawing.Size(65, 16);
+            this.rbDetails.TabIndex = 3;
+            this.rbDetails.TabStop = true;
+            this.rbDetails.Text = "Details";
+            this.rbDetails.UseVisualStyleBackColor = true;
+            // 
+            // rbTitle
+            // 
+            this.rbTitle.AutoSize = true;
+            this.rbTitle.Location = new System.Drawing.Point(28, 126);
+            this.rbTitle.Name = "rbTitle";
+            this.rbTitle.Size = new System.Drawing.Size(53, 16);
+            this.rbTitle.TabIndex = 4;
+            this.rbTitle.TabStop = true;
+            this.rbTitle.Text = "Title";
+            this.rbTitle.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(256, 280);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // imgListSmall
+            // 
+            this.imgListSmall.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imgListSmall.ImageSize = new System.Drawing.Size(32, 32);
+            this.imgListSmall.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imgListLarge
+            // 
+            this.imgListLarge.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imgListLarge.ImageSize = new System.Drawing.Size(32, 32);
+            this.imgListLarge.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 317);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.lvFilesFolders);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvFilesFolders;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbTitle;
+        private System.Windows.Forms.RadioButton rbDetails;
+        private System.Windows.Forms.RadioButton rbList;
+        private System.Windows.Forms.RadioButton rbSmallIcon;
+        private System.Windows.Forms.RadioButton rbLargeIcon;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.ImageList imgListSmall;
+        private System.Windows.Forms.ImageList imgListLarge;
+    }
+}
+
