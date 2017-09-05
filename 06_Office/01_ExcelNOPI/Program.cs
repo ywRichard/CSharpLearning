58ecd6a02f2eb40e54ea93643e010d13c655ac98
Victor's MacOS <ywfire0620@163.com>
2017-09-05 22:30:12 +0800

    Excel读写完成

--- 06_Office/01_ExcelNOPI/Program.cs
+++ 06_Office/01_ExcelNOPI/Program.cs
@@ -13,24 +13,28 @@ namespace _01_ExcelNOPI
         static void Main(string[] args)
         {
             ReadExcel("1.xls");
+            WriteExcel("1.xls");
+
+            Console.ReadKey();
         }
 
         private static void ReadExcel(string fileName)
         {
             using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
             {
-                var work = new HSSFWorkbook();//获取工作表
+                var work = new HSSFWorkbook(fs);//获取工作表
                 var sheet = work.GetSheetAt(0);//获取第一张表
 
-                var str = "";
                 for (int i = 0; i <= sheet.LastRowNum; i++)
                 {
-                    var row = sheet.GetRow(i);
+                    var row = sheet.GetRow(i);//获取行
+                    var str = "";
                     for (int j = 0; j < row.LastCellNum; j++)
                     {
+                        //Excel表中值有数据类型
                         if (row.GetCell(j).CellType == HSSFCell.CELL_TYPE_STRING)
                         {
-                            str += row.GetCell(j).StringCellValue;
+                            str += row.GetCell(j).StringCellValue;//获取单元格的值，类型转换
                         }
                         else if (row.GetCell(j).CellType == HSSFCell.CELL_TYPE_NUMERIC)
                         {
@@ -38,10 +42,37 @@ namespace _01_ExcelNOPI
                         }
                         str += ",";
                     }
-
+                    Console.WriteLine(str);
                 }
-                Console.WriteLine(str);
             }
+        }
+
+        private static void WriteExcel(string fileName)
+        {
+            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                var list = new List<Person>(){
+                    new Person(){ Name="张三",Age=15,Gender="男"},
+                    new Person(){ Name="李四",Age=16,Gender="女"},
+                    new Person(){ Name="王五",Age=17,Gender="女"},
+                    new Person(){ Name="赵六",Age=18,Gender="男"}
+                };
+
+                var work = new HSSFWorkbook();
+                var sheet = work.CreateSheet("student");
+                for (int i = 0; i < list.Count; i++)
+                {
+                    //行要根据页创建
+                    var row = sheet.CreateRow(i);//指定行号
+                    row.CreateCell(0, HSSFCell.CELL_TYPE_STRING).SetCellValue(list[i].Name);//指定单元格纵坐标
+                    row.CreateCell(1, HSSFCell.CELL_TYPE_STRING).SetCellValue(list[i].Gender);
+                    row.CreateCell(2, HSSFCell.CELL_TYPE_STRING).SetCellValue(list[i].Age);
+                }
+
+                work.Write(fs);
+            }
+
+            Console.WriteLine("写入完成");
         }
 
         public class Person
