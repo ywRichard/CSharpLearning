d07863d78c134b363edc8ee98aad90b2924d5c86
Victor's Office <ywfire0620@163.com>
2017-09-27 16:58:00 +0800

    Update

---
+++ 04_ASP/Itcaster.Web.App/2017_9_26/CookieDemo_1.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CookieDemo_1.aspx.cs" Inherits="Itcaster.Web._2017_9_26.CookieDemo_1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 04_ASP/Itcaster.Web.App/2017_9_26/CookieDemo_1.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Itcaster.Web._2017_9_26
+{
+    public partial class CookieDemo_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //1.
+            Response.Cookies["cp1"].Value = DateTime.Now.ToString();
+        }
+    }
+}
