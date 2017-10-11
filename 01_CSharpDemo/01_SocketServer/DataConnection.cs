7023f68fb0f22d59267b4abcf4b8e4d4206cca6e
Victor's MacOS <ywfire0620@163.com>
2017-10-11 22:21:44 +0800

    动态响应处理程序

--- 01_CSharpDemo/01_SocketServer/DataConnection.cs
+++ 01_CSharpDemo/01_SocketServer/DataConnection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net.Sockets;
+using System.Reflection;
 
 namespace _01_SocketServer
 {
@@ -60,25 +61,43 @@ namespace _01_SocketServer
                 case ".jpg":
                     ProcessStaticPage(request.FilePath);//对静态资源进行处理
                     break;
+                case ".aspx":
+                    ProcessDynamicPage(request.FilePath);
+                    break;
                 default:
                     break;
             }
         }
 
+        private void ProcessDynamicPage(string filePath)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(filePath);//获取没有扩展名的文件名
+            var nameSpace = Assembly.GetExecutingAssembly().GetName().Name;//获取程序集名称（命名空间）
+            var fullClassName = "_" + nameSpace + "." + fileName;//类的全称
+            object obj = Assembly.GetExecutingAssembly().CreateInstance(fullClassName);//创建对象
+            //var list = obj as List;
+            var list = obj as IHttpHandlerModel;
+            var msg = list.RenderHtml();
+            var buffer = System.Text.Encoding.UTF8.GetBytes(msg);
+            var response = new HttpResponse(buffer, Path.GetExtension(filePath));
+            newSocket.Send(response.GetHeaderResponse());//响应报文头
+            newSocket.Send(buffer);
+        }
+
         private void ProcessStaticPage(string fileName)
         {
             var dataDir = AppDomain.CurrentDomain.BaseDirectory;
-            if (dataDir.EndsWith(@"\bin\Debug\")||dataDir.EndsWith(@"\bin\Release\"))
+            if (dataDir.EndsWith(@"\bin\Debug\") || dataDir.EndsWith(@"\bin\Release\"))
             {
                 dataDir = Directory.GetParent(dataDir).Parent.Parent.FullName;
             }
             var fullDir = dataDir + fileName;//获取处理的完整文件路径
-            using (var fileStream=new FileStream(fullDir,FileMode.Open,FileAccess.Read))
+            using (var fileStream = new FileStream(fullDir, FileMode.Open, FileAccess.Read))
             {
                 var buffer = new byte[fileStream.Length];
-                int length = fileStream.Read(buffer,0,buffer.Length);//读取形态文件内容
+                int length = fileStream.Read(buffer, 0, buffer.Length);//读取形态文件内容
                 //构建响应报文
-                HttpResponse response = new HttpResponse(buffer,Path.GetExtension(fullDir));
+                var response = new HttpResponse(buffer, Path.GetExtension(fullDir));
                 newSocket.Send(response.GetHeaderResponse());//发送请求报文头
                 newSocket.Send(buffer);
             }
---
+++ 01_CSharpDemo/01_SocketServer/List.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace _01_SocketServer
+{
+    internal class List : IHttpHandlerModel
+    {
+        public string RenderHtml()
+        {
+            var dir = AppDomain.CurrentDomain.BaseDirectory;
+            if (dir.EndsWith(@"\bin\Debug\") || dir.EndsWith(@"\bin\Release\"))
+            {
+                dir = Directory.GetParent(dir).Parent.Parent.FullName;
+            }
+            var fullDir = dir + "/model.html";
+            var fileContent = File.ReadAllText(fullDir);
+            return fileContent.Replace("$title", "新闻").Replace("$body", "新闻列表页面");
+        }
+    }
+}
