01175623f85b214db91293f283b38d4bb84f0b9f
Victor's MacOS <ywfire0620@163.com>
2017-10-25 20:22:04 +0800

    Update

---
+++ 04_EntityFramwork/02_CodeFirst/BookInfo1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_CodeFirst
+{
+    public class BookInfo1
+    {
+
+        [Key]
+        public int BId { get; set; }
+        [StringLength(50)]
+        public string BookTitle { get; set; }
+        [ForeignKey("BookType1")]
+        public int TypeId { get; set; }
+
+        public virtual BookType1 BookType1 { get; set; }
+    }
+}
