ad1df5c926261dfd505b96d917b17137ba4f0135
Victor's Office <ywfire0620@163.com>
2017-11-13 16:51:38 +0800

    多条件查询未结束

--- 07_MvcOA/07_MvcOA.WebApp/Controllers/UserInfoController.cs
+++ 07_MvcOA/07_MvcOA.WebApp/Controllers/UserInfoController.cs
@@ -12,7 +12,7 @@ namespace _07_MvcOA.WebApp.Controllers
     public class UserInfoController : Controller
     {
         // GET: UserInfo
-        IUserInfoBLL userInfoBll=new UserInfoBLL();
+        IUserInfoBLL userInfoBll = new UserInfoBLL();
         public ActionResult Index()
         {
             return View();
@@ -25,7 +25,19 @@ namespace _07_MvcOA.WebApp.Controllers
         {
             var pageIndex = Request["page"] != null ? int.Parse(Request["page"]) : 1;
             var pageSize = Request["rows"] != null ? int.Parse(Request["rows"]) : 5;
-            int totalCount;
+            var name = Request["name"];
+            var remark = Request["remark"];
+            //构建搜索条件
+            int totalCount = 0;
+            var userInfoParam = new UserInfoParam()
+            {
+                UserName = name,
+                Remark = remark,
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+
             short delFlag = (short)DelFlagEnum.Normal;
             var userInfoList = userInfoBll.LoadPageEntities(pageIndex, pageSize, out totalCount,
                 c => c.DelFlag == delFlag,
@@ -39,8 +51,11 @@ namespace _07_MvcOA.WebApp.Controllers
                 RegTime = u.SubTime,
             });
 
-            return Json(new {
-                rows = temp, total = totalCount },
+            return Json(new
+            {
+                rows = temp,
+                total = totalCount
+            },
                 JsonRequestBehavior.AllowGet
                 );
         }
@@ -67,32 +82,43 @@ namespace _07_MvcOA.WebApp.Controllers
                 return Content("no");
             }
         }
-
+        /// <summary>
+        /// 添加用户信息
+        /// </summary>
+        /// <param name="userInfo"></param>
+        /// <returns></returns>
         public ActionResult AddUserInfo(UserInfo userInfo)
         {
             userInfo.DelFlag = 0;
             userInfo.SubTime = DateTime.Now;
             userInfo.ModifiedOn = DateTime.Now;
             userInfoBll.AddEntity(userInfo);
-            
+
             return Json("ok");
         }
-
+        /// <summary>
+        /// 通过ID查询用户信息
+        /// </summary>
+        /// <returns></returns>
         public ActionResult GetUserInfoModel()
         {
             int id = int.Parse(Request["id"]);
             var userInfo = userInfoBll.LoadEntities(c => c.UserID == id).FirstOrDefault();
 
-            if (userInfo!=null)
-                return Json(new { serverData=userInfo,msg="ok"},JsonRequestBehavior.AllowGet);
+            if (userInfo != null)
+                return Json(new { serverData = userInfo, msg = "ok" }, JsonRequestBehavior.AllowGet);
             else
                 return Json(new { msg = "no" }, JsonRequestBehavior.AllowGet);
         }
-
+        /// <summary>
+        /// 编辑用户信息
+        /// </summary>
+        /// <param name="userInfo"></param>
+        /// <returns></returns>
         public ActionResult EditUserInfo(UserInfo userInfo)
         {
             userInfo.ModifiedOn = DateTime.Now;
-            if(userInfoBll.EditEntity(userInfo))
+            if (userInfoBll.EditEntity(userInfo))
             {
                 return Content("ok");
             }
