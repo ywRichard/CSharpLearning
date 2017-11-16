41cb803bfcbd55be04a36a61c06b80e4d3a457c8
Victor's MacOS <ywfire0620@163.com>
2017-11-16 19:40:45 +0800

    跳转验证

---
+++ 07_MvcOA/07_MvcOA.WebApp/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _07_MvcOA.WebApp.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            if (Session["UserInfo"]==null)
+            {
+                filterContext.HttpContext.Response.Redirect("/Login/Index");
+            }
+        }
+    }
+}
