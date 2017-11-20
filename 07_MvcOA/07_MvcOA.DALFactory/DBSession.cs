c76f2d1402d53fc2159041aed871a05c69998773
Victor's Office <ywfire0620@163.com>
2017-11-20 17:00:30 +0800

    Update T4 Template

--- 07_MvcOA/07_MvcOA.DALFactory/DBSession.cs
+++ 07_MvcOA/07_MvcOA.DALFactory/DBSession.cs
@@ -14,23 +14,14 @@ namespace _07_MvcOA.DALFactory
     /// DBSession：工厂类（数据会话层）
     /// 作用：创建数据操作类的实例，业务层通过DBSession调用相应的数据操作类的实例，这样的业务层和数据层解耦。
     /// </summary>
-    public class DBSession : IDBSession
+    public partial class DBSession : IDBSession
     {
         public DbContext Context
         {
             get { return DbContextFactory.CreateDbContext(); }
         }
         private IUserInfoDal _userInfoDal;
-        public IUserInfoDal UserInfoDal
-        {
-            get
-            {
-                if (_userInfoDal == null)
-                    _userInfoDal = AbstractFactory.CreateUserInfoDal();//如果更换数据层，这里也要更换
-                return _userInfoDal;
-            }
-            set { _userInfoDal = value; }
-        }
+
         /// <summary>
         /// 一个业务中有可能涉及到对多张表的操作，那么可以将操作的数据传递到数据层中相应的方法，打上相应的标记，最后调用该方法。
         /// 将数据一次性递交到数据库中，避免多次链接数据库
---
+++ 07_MvcOA/07_MvcOA.DALFactory/DbSession1.cs
@@ -0,0 +1,32 @@
+﻿ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _07_MvcOA.DAL;
+using _07_MvcOA.IDAL;
+using _07_MvcOA.Model;
+using System.Data.Entity;
+
+namespace _07_MvcOA.DALFactory
+{
+	public partial class DBSession : IDBSession
+    {
+	
+		private IUserInfoDal _UserInfoDal;
+        public IUserInfoDal UserInfoDal
+        {
+            get
+            {
+                if(_UserInfoDal == null)
+                {
+                    _UserInfoDal = AbstractFactory.CreateUserInfoDal();
+                }
+                return _UserInfoDal;
+            }
+            set { _UserInfoDal = value; }
+        }
+	}	
+}
