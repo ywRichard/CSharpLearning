f9aabbc1293426e7a130f395d2d9e8fa46d8ef70
Victor's MacOS <ywfire0620@163.com>
2017-11-21 22:41:27 +0800

    update

---
+++ 07_MvcOA/07_MvcOA.Model/SearchPattern/RoleInfoParam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_MvcOA.Model
+{
+    public class RoleInfoParam : BaseParam
+    {
+        public string Name { get; set; }
+        public string Remark { get; set; }
+    }
+}
--- 07_MvcOA/07_MvcOA.WebApp/Controllers/RoleInfoController.cs
+++ 07_MvcOA/07_MvcOA.WebApp/Controllers/RoleInfoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using _07_MvcOA.IBLL;
+using _07_MvcOA.Model;
 
 namespace _07_MvcOA.WebApp.Controllers
 {
@@ -18,22 +19,40 @@ namespace _07_MvcOA.WebApp.Controllers
 
         public ActionResult GetRoleInfo()
         {
-            return View();
+            var pageIndex = Request["page"] != null ? int.Parse(Request["page"]) : 1;
+            var pageSize = Request["rows"] != null ? int.Parse(Request["rows"]) : 5;
+            var name = Request["name"];
+            var remark = Request["remark"];
+            //构建搜索条件
+            var totalCount = 0;
+            var delFlag = (short)DelFlagEnum.Normal;
+            var roleInfoList = RoleInfoBll.LoadPageEntities(pageIndex, pageSize, out totalCount, r => r.DelFlag == delFlag, r => r.ID, true);
+            var temp = from r in roleInfoList
+                       select new
+                       {
+                           ID = r.ID,
+                           RoleName = r.RoleName,
+                           Sort = r.Sort,
+                           SubTime = r.SubTime,
+                           Remark = r.Remark
+                       };
+
+            return Json(new { rows = temp, total = totalCount }, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult DeleteRoleInfo()
-        {
-            return Content();
-        }
+        //public ActionResult DeleteRoleInfo()
+        //{
+        //    return Content();
+        //}
 
-        public ActionResult AddRoleInfo()
-        {
-            return Json();
-        }
+        //public ActionResult AddRoleInfo()
+        //{
+        //    return Json();
+        //}
 
-        public ActionResult EditRoleInfo()
-        {
-            return Content();
-        }
+        //public ActionResult EditRoleInfo()
+        //{
+        //    return Content();
+        //}
     }
 }
