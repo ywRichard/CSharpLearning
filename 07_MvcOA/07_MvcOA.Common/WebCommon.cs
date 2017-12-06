9b5e90072bffb631155783ccd05b074de9dc6155
Victor's Office <ywfire0620@163.com>
2017-12-06 17:12:37 +0800

    Update

--- 07_MvcOA/07_MvcOA.Common/WebCommon.cs
+++ 07_MvcOA/07_MvcOA.Common/WebCommon.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Tokenattributes;
+using PanGu;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace _07_MvcOA.Common
 {
@@ -26,6 +31,45 @@ namespace _07_MvcOA.Common
             }
             md5.Clear();
             return sb.ToString();
+        }
+
+        public static void GoPage()
+        {
+            HttpContext.Current.Response
+                .Redirect("/Login/Index/?returnUrl=" + HttpContext.Current.Request.Url.ToString());
+        }
+        /// <summary>
+        /// 对输入的搜索条件进行分词
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static List<string> GetPanGuWord(string str)
+        {
+            Analyzer analyzer = new PanGuAnalyzer();
+
+            TokenStream tokenStream = analyzer.TokenStream("", new StringReader(str));
+
+            var list = new List<string>();
+            while (tokenStream.IncrementToken())
+            {
+                var term = tokenStream.GetAttribute<ITermAttribute>();
+                list.Add(term.Term);
+            }
+
+            return list;
+        }
+
+        public static string CreateHighLight(string keywords, string content)
+        {
+            //创建HTMLFormatter,参数为高亮单词的前后缀
+            PanGu.HighLight.SimpleHTMLFormatter simpleHTMLFormatter =
+            new PanGu.HighLight.SimpleHTMLFormatter("<font color=\"red\">", "</font>");//同过改变字体颜色，实现高亮显示
+            //创建 Highlighter ，输入HTMLFormatter 和 盘古分词对象Semgent
+            PanGu.HighLight.Highlighter highlighter = new PanGu.HighLight.Highlighter(simpleHTMLFormatter, new Segment());
+            //设置每个摘要段的字符数
+            highlighter.FragmentSize = 50;
+            //获取最匹配的摘要段
+            return highlighter.GetBestFragment(keywords, content);
         }
     }
 }
--- 07_MvcOA/07_MvcOA.WebApp/Controllers/SearchController.cs
+++ 07_MvcOA/07_MvcOA.WebApp/Controllers/SearchController.cs
@@ -2,6 +2,7 @@
 using Lucene.Net.Analysis;
 using Lucene.Net.Documents;
 using Lucene.Net.Index;
+using Lucene.Net.Search;
 using Lucene.Net.Store;
 using System;
 using System.Collections.Generic;
@@ -25,7 +26,10 @@ namespace _07_MvcOA.WebApp.Controllers
         {
             if (!string.IsNullOrEmpty(Request["btnSearch"]))
             {
-
+                var list = SearchBookContent();
+                ViewData["searchList"] = list;
+                ViewData["searchWhere"] = Request["txtContent"];//重新递交页面之后，在搜索框中保留搜索的关键字，
+                return View("Index");
             }
             else
             {
@@ -34,8 +38,11 @@ namespace _07_MvcOA.WebApp.Controllers
 
             return Content("ok");
         }
-
-        public ActionResult CreateSearchIndex()
+        /// <summary>
+        /// 负责向Lucene.Net中写数据
+        /// </summary>
+        /// <returns></returns>
+        private void CreateSearchIndex()
         {
             var indexPath = @"C:\Users\Victor\Desktop\LuceneNetDir";//注意和磁盘上文件夹的大小写一致，否则会出现报错，将创建的分词内容放在该目录下。
             FSDirectory directory = FSDirectory.Open(new DirectoryInfo(indexPath), new NativeFSLockFactory());//指定索引文件（打开索引目录） FS指的就是FileSystem
@@ -50,7 +57,6 @@ namespace _07_MvcOA.WebApp.Controllers
                 }
             }
             IndexWriter writer = new IndexWriter(directory, new PanGuAnalyzer(), !isUpdate, Lucene.Net.Index.IndexWriter.MaxFieldLength.UNLIMITED);//向索引库中写索引，这时在这里加锁。
-
 
             foreach (Book bookModel in BookBll.LoadEntities(b => true))
             {
@@ -67,8 +73,43 @@ namespace _07_MvcOA.WebApp.Controllers
 
             writer.Dispose();//会自动解锁
             directory.Dispose();//不要忘了close，否则索引结果搜不到
+        }
 
-            return Content("ok");
+        private List<Models.ViewSearchContentModel> SearchBookContent()
+        {
+            var indexPath = @"C:\Users\Victor\Desktop\LuceneNetDir";
+            var kw = Common.WebCommon.GetPanGuWord(Request["txtContent"]);
+            FSDirectory directory = FSDirectory.Open(new DirectoryInfo(indexPath), new NoLockFactory());
+            IndexReader reader = IndexReader.Open(directory, true);
+            IndexSearcher searcher = new IndexSearcher(reader);
+            //搜索条件
+            PhraseQuery query = new PhraseQuery();
+            foreach (var word in kw)//先用空格，让用户去分词，空格分隔的就是词"计算机   专业"
+            {
+                query.Add(new Term("body", word));
+            }
+            //WITH_POSITIONS_OFFSETS -> 盘古分词在建立索引的时候已经将干扰词间距记录，lucene.net只要匹配就可以了
+            query.Slop = 100;//多个查询条件词之间的最大距离，在文章中相隔太远也就无意义（例如 “大学生”这个查询条件和“简历”这个查询条件之间如果间隔的词太多也就没有意义了。）
+            //TopScoreDocCollector是盛放查询结果的容器
+            TopScoreDocCollector collector = TopScoreDocCollector.Create(1000, true);
+            searcher.Search(query, null, collector);//根据query查询条件进行查询，查询结果放入cllector容器
+            ScoreDoc[] docs = collector.TopDocs(0, collector.TotalHits).ScoreDocs;//得到所有查询结果中的文档，collector.TotalHits:表示总条数；TopDocs(300,20):表示得到300(从300开始)，到320(结束）的文档内容
+            var list = new List<Models.ViewSearchContentModel>();
+            //可以用来实现分页内容
+            for (int i = 0; i < docs.Length; i++)
+            {
+                //搜索ScoreDoc[]只能获得文档的id，这样不会把查询结果的Document一次性加载到内存中。降低了内存的压力，需要获得文档的详细内容的时候通过searcher.Doc来根据文档id来获得文档的详细内容对象document
+                int docId = docs[i].Doc;//得到查询结果文档的id(Lucene内容分配的id)
+                Document doc = searcher.Doc(docId);//找到文档id对应的文档详细信息
+                list.Add(new Models.ViewSearchContentModel
+                {
+                    Id = doc.Get("Id"),
+                    Title = doc.Get("Title"),
+                    Content = Common.WebCommon.CreateHighLight(Request["txtContent"], doc.Get("Content"))
+                });
+            }
+
+            return list;
         }
     }
 }
