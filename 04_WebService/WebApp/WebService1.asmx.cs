e52cbf6bf88f3c83ccdfeae718f18127d30059cf
Victor's MacOS <ywfire0620@163.com>
2017-12-10 18:06:42 +0800

    Update WebService

---
+++ 04_WebService/WebApp/WebService1.asmx.cs
@@ -0,0 +1,46 @@
+﻿using _07_MvcOA.Model;
+using Spring.Context;
+using Spring.Context.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using _07_MvcOA.IBLL;
+using _07_MvcOA.Common;
+
+namespace WebApp
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod(Description = "计算两个数的和")]//曝露API
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        [WebMethod]
+        public string GetUserInfoList()
+        {
+            //序列化失败的原因是WebService返回值通过Http传递，不能传输类。所以要将返回值序列化。
+            IApplicationContext context = ContextRegistry.GetContext();
+            var userInfoBll = (IUserInfoBLL)context.GetObject("UserInfoBLL");
+            var list = userInfoBll.LoadEntities(u => u.DelFlag == 0).ToList();
+
+            return SerializerHelper.SerializerToString(list);
+        }
+    }
+}
---
+++ 04_WebService/WebForm/Index.aspx.cs
@@ -0,0 +1,46 @@
+﻿using _07_MvcOA.Model;
+using _07_MvcOA.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebForm.WeatherReference;
+
+namespace WebForm
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient client = new ServiceReference1.WebService1SoapClient();
+            //var result = client.Add(1,6);
+            //Response.Write(result.ToString());
+
+            var str = client.GetUserInfoList();
+            List<UserInfo> list = SerializerHelper.DeserializerToObject<List<UserInfo>>(str);
+            foreach (var userInfo in list)
+            {
+                Response.Write(userInfo.UserName);
+                Response.Write("</br>");
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            var client = new WeatherWebServiceSoapClient();
+            var strs = client.getSupportCity("");
+            foreach (var str in strs)
+            {
+                Response.Write(str);
+                Response.Write("</br>");
+            }
+        }
+    }
+}
