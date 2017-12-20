baf446bced00a9de9b2543f09ffa196c267ae4c9
Victor's Office <ywfire0620@163.com>
2017-12-20 17:13:30 +0800

    Update

---
+++ 01_CSharpDemo/Charper6_Iterator/IterationSample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charper6_Iterator
+{
+    /// <summary>
+    /// C#1中手动实现的迭代器简化模型
+    /// </summary>
+    public class IterationSample : IEnumerable
+    {
+        private object[] _values;
+        private int _startingPoint;
+
+        public IterationSample(object[] values, int startingPoint)
+        {
+            _values = values;
+            _startingPoint = startingPoint;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new IterationSampleIterator(this);
+        }
+        /// <summary>
+        /// 嵌套类可以访问它外层类型的私有成员
+        /// </summary>
+        class IterationSampleIterator : IEnumerator
+        {
+            private readonly IterationSample _parent;
+            private int _position;
+
+            internal IterationSampleIterator(IterationSample parent)
+            {
+                _parent = parent;
+                _position = -1;
+            }
+
+            public bool MoveNext()
+            {
+                if (_position != _parent._values.Length)
+                {
+                    _position++;
+                }
+
+                return _position < _parent._values.Length;
+            }
+
+            public void Reset()
+            {
+                _position = -1;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    if (_position == -1 || _position == _parent._values.Length)
+                    {
+
+                    }
+                    var index = _position + _parent._startingPoint;
+                    index = index % _parent._values.Length;
+                    return _parent._values[index];
+                }
+            }
+        }
+    }
+}
---
+++ 01_CSharpDemo/Charper6_Iterator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charper6_Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TryIterationSampple();
+        }
+
+        #region IterationSample
+
+        static void TryIterationSampple()
+        {
+            object[] values = { "a", "b", "c", "d", "e" };
+            IterationSample collection = new IterationSample(values, 3);
+            foreach (object x in collection)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        #endregion
+    }
+}
