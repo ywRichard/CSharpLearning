6bf33ff4d2c36bc339a03fc6bc89a51bf9ddfc2a
Victor's Office <ywfire0620@163.com>
2017-12-22 17:01:43 +0800

    Update

---
+++ 01_CSharpDemo/Chapter07/PartialMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter07
+{
+    partial class PartialMethod
+    {
+        public PartialMethod()
+        {
+            OnConstructorStart();//没有任何实现的部分方法，编译器会把他移除
+            Console.WriteLine("Generated Constructor");
+            OnConstructorEnd();
+        }
+
+        partial void OnConstructorStart();
+        partial void OnConstructorEnd();
+    }
+
+    partial class PartialMethod
+    {
+        partial void OnConstructorEnd()
+        {
+            Console.WriteLine("Manual code");
+        }
+    }
+}
---
+++ 01_CSharpDemo/Chapter07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TryPartialMethod();
+        }
+
+        static void TryPartialMethod()
+        {
+            var pm = new PartialMethod();
+        }
+    }
+}
